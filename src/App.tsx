138d575c0b40dbc73ff7230b5bdd1a31f2d091b7
kovaaalex <kovalenko.alex04@gmail.com>
2025-09-05 21:55:00 +0300

    feat: add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router'
+import LoginPage from './pages/LoginPage/LoginPage'
+import HomePage from './pages/HomePage/HomePage'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Coincap</h1>
-    </>
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage/>}></Route>
+          <Route path='/coincap/login' element={<LoginPage/>}></Route>
+        </Routes>
+      </Router>
   )
 }
 
---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import type { LoginForm } from "../../types/login.types";
+import styles from './LoginPage.module.css';
+
+const LoginPage = () => {
+    const { register, handleSubmit, formState: {errors} } = useForm<LoginForm>();
+    const [data, setData] = useState('');
+    const onSubmit = (data : LoginForm) => {
+        setData(JSON.stringify(data));
+    }
+    return(
+        <div className={styles.login__page}>
+            <form className={styles.login__form} onSubmit={handleSubmit(onSubmit)}>
+                <div className={styles.input__container}>
+                    <input {...register("login", {
+                        required: "Login is required",
+                        minLength: {
+                            value: 4,
+                            message: 'Must be at least 4 characters'
+                        },
+                        maxLength: {
+                            value: 10,
+                            message: 'Must be at max 10 characters'
+                        },
+                    })}
+                        type="text" 
+                        placeholder="Login"
+                        className={errors.login ? styles.error__input : styles.input}
+                    />
+                    {errors.login && <span className={styles.error}>{errors.login.message}</span>}
+                </div>
+                <div className={styles.input__container}>
+                    <input {...register("password", {
+                        required: "Password is required",
+                        minLength: {
+                            value: 4,
+                            message: 'Must be at least 4 characters'
+                        },
+                        maxLength: {
+                            value: 10,
+                            message: 'Must be at max 10 characters'
+                        },
+                    })} 
+                        type="password" 
+                        placeholder="Password"
+                        className={errors.password ? styles.error__input : styles.input}
+                    />
+                    {errors.password && <span className={styles.error}>{errors.password.message}</span>}
+                </div>
+                <input type="submit" />
+            </form>
+        </div>
+    )
+}
+export default LoginPage;
