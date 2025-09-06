be85fc29f148b1f8f528a21f23ea9a55fdd5ae30
kovaaalex <kovalenko.alex04@gmail.com>
2025-09-06 16:38:21 +0300

    feat: add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+// import { Link } from "react-router";
+
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import styles from './Header.module.css';
+const Header = () => {
+    const navigate = useNavigate();
+    const { logout } = useAuth();
+    const handleLogout = () => {
+        logout();
+        navigate('/coincap/login');
+    }
+    return(
+        <header className={styles.header}>
+            <button onClick={handleLogout}>Logout</button>
+        </header>
+    )
+}
+export default Header;
