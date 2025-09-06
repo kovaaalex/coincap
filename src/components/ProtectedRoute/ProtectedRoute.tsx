798de0025f828e1b0b96272daf65be5aa2291a9e
kovaaalex <kovalenko.alex04@gmail.com>
2025-09-06 13:59:18 +0300

    Revert "Merge pull request #1 from kovaaalex/feature/login-auth"
    This reverts commit f498a28bac784c9283aa530a6092f2735c16318e, reversing
    changes made to 621a7a6813116278c7d3aa80144f0f32dfd77b8d.

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router";
-import useAuth from "../../hooks/useAuth";
-
-const ProtectedRoute: React.FC<{children: React.ReactNode}> = ({children}) => {
-    const { isAuthenticated } = useAuth();
-    return isAuthenticated() ? <>{children}</> : <Navigate to="/coincap/login" replace />
-}
-export default ProtectedRoute;
