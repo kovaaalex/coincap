8ffa43c30ea66c5dd62da2c535e510ead46c64c0
kovaaalex <kovalenko.alex04@gmail.com>
2025-09-09 14:22:51 +0300

    feat: add input search

---
+++ src/components/InputSearch/InputSearch.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import styles from './InputSearch.module.css';
+import type { SearchInputProps } from "../../types/search.types";
+
+const SearchInput: React.FC<SearchInputProps> = ({
+    onSearch, 
+    placeholder, 
+    isLoading = false, 
+}) => {
+    const [inputValue, setInputValue] = useState("");
+    const handleSearch = () => {
+        onSearch(inputValue.trim());
+    };
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    };
+    return (
+        <div className={styles.searchContainer}>
+            <div className={styles.searchInputWrapper}>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleChange}
+                    placeholder={placeholder}
+                    className={styles.searchInput}
+                    disabled={isLoading}
+                />
+                <button
+                    type="button"
+                    onClick={handleSearch}
+                    className={styles.searchButton}
+                    disabled={isLoading}
+                    aria-label="Search"
+                >
+                    Search
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default SearchInput;
